dfbb9c3aa46fcd77e9bed8fa167db63b4b8af5ad
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-08-21 10:53:35 +0900

    🚚 :: 레벨 나누기

---
+++ programmers/lv2/기능개발.py
@@ -0,0 +1,19 @@
+def solution(progresses, speeds):
+
+    count = []
+
+    i = 0
+    while True:
+
+        progresses = [x+y for x,y in zip(progresses, speeds)]
+
+        if progresses[i] >= 100:
+            count.append(0)
+
+            while progresses[i] >= 100:
+
+                i += 1
+                count[len(count) - 1] += 1
+
+                if i >= len(progresses):
+                    return count
